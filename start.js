abe37e9c4afb7a9d6f07089d19b1b76f3e98cf2d
karin yahud <karin167bz@gmail.com>
2021-04-28 15:21:54 +0200

    complit the task

--- start.js
+++ start.js
@@ -15,3 +15,33 @@ const userDataDump = [
   "D,Cheesedude51,1951,Users",
   "E,cron,,System",
 ];
+
+
+const userObject = str => {
+    const data = str.split(",");
+    for (let i = 0; i < data.length; i++) {
+        if (!data[i]) {
+            data[i] = "Unknown";
+        }
+        if (i === data.length - 1) {
+            const departments = data[data.length - 1].split("|");
+            data[data.length - 1] = departments;
+        }
+    }
+    return {
+        id: data[0],
+        fullname: data[1],
+        birthYear: data[2],
+        departments: data[3],
+    };
+};
+
+const userFactory = arr => {
+    const users = [];
+    for (let i = 0; i < arr.length; i++) {
+        users.push(userObject(arr[i]));
+    }
+    return users;
+};
+
+console.log(userFactory(userDataDump));
